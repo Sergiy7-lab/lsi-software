ab2345a66d8abc0e47a4a49265df20bed5b4ec84
Sergio Mikhalev <sergiy.mihalev@gmail.com>
2022-07-08 00:11:00 +0300

    second commit

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Tables} from "../table.interface";
 import {HttpClient} from "@angular/common/http";
 import {Table} from "primeng/table";
@@ -14,7 +14,7 @@ export class TableComponent implements OnInit {
 
   table: Tables[];
   dateSelected: any;
-
+  formatDate: string;
 
   constructor(private http: HttpClient, private themeService: ThemeService) {
   }
@@ -23,34 +23,35 @@ export class TableComponent implements OnInit {
     this.getTable();
   }
 
-
-
   getDateString (){
     const format = (date) => date < 10 ? `0${date}` : date.toString();
     const date = new Date(this.dateSelected);
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
-    return `${year}-${format(month)}-${format(day)}`;
+    return this.formatDate = `${year}-${format(month)}-${format(day)}`;
   };
 
   getDate() {
-    const formatDate = this.getDateString();
-    console.log(formatDate)
-    this.http.get<any>(`http://api.nbp.pl/api/exchangerates/tables/A/${formatDate}/?format=json`).subscribe(
-      response => {
-        this.table = response[0].rates;
-        console.log(response[0].rates)
-      }
-    )
+    this.getDateString();
+    if (this.formatDate !== "NaN-NaN-NaN") {
+      this.http.get<any>(`https://api.nbp.pl/api/exchangerates/tables/A/${this.formatDate}/?format=json`).subscribe(
+        response => {
+          this.table = response[0].rates;
+        }, error => {
+          console.log(error)
+        }
+      )
+    }
+    return;
   }
 
   getTable() {
     this.http.get<any>('https://api.nbp.pl/api/exchangerates/tables/A/?format=json').subscribe(
       response => {
         this.table = response[0].rates;
-        console.log(response[0].rates)
-
+      }, error => {
+        console.log(error)
       }
     )
   }
@@ -59,12 +60,9 @@ export class TableComponent implements OnInit {
     table.clear();
     this.dateSelected = Date.now();
     this.getDate();
-
   }
 
-
   changeTheme(theme: string) {
-    console.log(theme)
     this.themeService.switchTheme(theme)
   }
 }
